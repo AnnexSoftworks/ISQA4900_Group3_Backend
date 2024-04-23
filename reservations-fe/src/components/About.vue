b06cd67b44e941b88ba806025afd2e25211172d7
Annex Softworks <78704624+AnnexSoftworks@users.noreply.github.com>
2024-04-22 22:25:08 -0500

    Updated the frontend

--- reservations-fe/src/components/About.vue
+++ reservations-fe/src/components/About.vue
@@ -1,5 +1,65 @@
 <template>
-  <div class="header-text">About Page</div>
+  <section class="banner-tems text-center">
+    <div class="container">
+      <div class="banner-content">
+        <h2 style="text-shadow: 1px 1px 4px #000000; margin-top: -88px;">Emerald Bay Island Resort</h2>
+        <p style="text-shadow: 1px 1px 4px #000000;">Where luxury harmonizes with the serene beauty of nature.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-about">
+    <div class="container">
+      <div class="row">
+        <div class="wrap-about">
+          <div class="about-item">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 no-padding-right">
+              <div class="section-slider height-v-about">
+                <div id="index12" class="owl-carousel  owl-theme">
+                  <div class="item">
+                    <img alt="Third slide" src="../assets/images/coco-cay01.webp" class="img-responsive">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 no-padding-left">
+              <div class="text">
+                <h2 class="heading">Our History</h2>
+                <div class="desc">
+                  <p>Emerald Bay Island Resort, in partnership with Royal Caribbean Cruise Lines, stands as a pinnacle
+                    of secluded luxury, nestled on the pristine shores of RCCL's Cocoa Cay. Our philosophy weaves the
+                    tranquility of an exclusive island getaway with the unmatched convenience of RCCL's impeccable
+                    voyages. While we share the island with RCCL, our resort is a world apart—a tranquil enclave
+                    dedicated to privacy, luxury, and an immersive natural experience.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="about-item about-right">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6  no-padding-left col-lg-push-6 col-md-push-6 ">
+              <div class="img">
+                <img src="../assets/images/coco-cay02.webp" alt="#" class="img-responsive">
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 no-padding-right col-lg-pull-6 col-md-pull-6">
+              <div class="text">
+                <h2 class="heading">Tropical Paradise</h2>
+                <div class="desc">
+                  <p>At Emerald Bay, we believe in creating unforgettable experiences that resonate with the unique
+                    essence of our tropical paradise. From the architectural elegance of our accommodations, which
+                    harmonize with the island’s natural splendor, to the personalized services designed to indulge your
+                    senses, every aspect of the resort is crafted to enchant and soothe. Whether it's a retreat in one
+                    of our Coral Studios, an escape to a Garden Bungalow, or a sumptuous stay in our Island Haven
+                    Villas, our guests enjoy exclusivity with a view, comfort in the lap of luxury, and nature right at
+                    their doorstep. Join us at Emerald Bay, where your island fantasy becomes a reality.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
--- reservations-fe/src/components/Contact.vue
+++ reservations-fe/src/components/Contact.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="header-text">Contact Page</div>
+  <section class="banner-tems text-center">
+    <div class="container">
+      <div class="banner-content">
+        <h2 style="margin-top: -88px;">Get in Touch</h2>
+        <p>Start Your Journey to Paradise.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-contact" style="margin-top: 44px;">
+    <div class="container">
+      <div class="contact">
+        <div class="row">
+          <div class="col-md-6 col-lg-5">
+            <div class="text">
+              <h2>24/7 Island Concierge</h2>
+              <p>At Emerald Bay Island Resort, your exceptional experience is our top priority. Whether you have
+                questions about your upcoming stay, need assistance with travel arrangements, or simply want to share
+                your thoughts, our dedicated team is eager to hear from you. Use the form below to send us a message, or
+                reach out directly via phone or email. We pledge to provide a timely and thoughtful response, ensuring
+                your journey to our island paradise is nothing short of perfect. For immediate assistance, our customer
+                service representatives are available 24/7 to cater to your needs. Connect with us today and take the
+                next step in planning your unforgettable island escape.</p>
+              <ul>
+                <li><i class=" fa ion-ios-location-outline"></i>121 Emerald Bay Dr., Bahamas</li>
+                <li><i class="fa fa-phone" aria-hidden="true"></i> (800) 555-8898</li>
+                <li><i class="fa fa-envelope-o" aria-hidden="true"></i>contact@emeraldbayresort.com</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 col-lg-offset-1">
+            <div class="contact-form">
+              <form action="send_mail_contact.php" method="post">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <input type="text" class="field-text" name="name" placeholder="Name">
+                  </div>
+                  <div class="col-sm-6">
+                    <input type="text" class="field-text" name="email" placeholder="Phone">
+                  </div>
+                  <div class="col-sm-12">
+                    <input type="text" class="field-text" name="subject" placeholder="Email">
+                  </div>
+                  <div class="col-sm-12">
+                    <textarea cols="30" rows="10" name="message" class="field-textarea"
+                              placeholder="Inquiry"></textarea>
+                  </div>
+                  <div class="col-sm-6">
+                    <button type="submit" class="btn btn-room">SEND</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,6 +68,6 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top:220px;
+  margin-top: 220px;
 }
 </style>
